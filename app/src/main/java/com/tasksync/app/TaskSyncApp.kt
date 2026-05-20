d686f7e6995bf2f066ae0b0c15ed34e47d7626d7
Ghofur Akbar <infamousg035@gmail.com>
2026-05-20 10:58:13 +0700

    adjustment for accomoding WorkManager + SyncWorker

--- app/src/main/java/com/tasksync/app/TaskSyncApp.kt
+++ app/src/main/java/com/tasksync/app/TaskSyncApp.kt
@@ -1,7 +1,19 @@
 package com.tasksync.app
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class TaskSyncApp : Application()
+class TaskSyncApp : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+}
